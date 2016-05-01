13666c61f64cd655e2b7b317fb02b83e85aedc21
Robert Millward <robert.millward@knights.ucf.edu>
2016-05-01 18:04:46 -0400

    same floyd but it makes a copy first because *shrug*

--- Graph/floyd_generic.java
+++ Graph/floyd_generic.java
@@ -4,8 +4,7 @@ public class floyd_generic {
 
     public static void main(String[] args){
         int[][] G;
-        int[][] D;
-
+        // example graph, normally you would read in from the problem
         G = new int[][]{
                 {0,1,2,3,4,25},
                 {oo,0,6,7,8,9},
@@ -15,6 +14,7 @@ public class floyd_generic {
                 {oo,oo,oo,oo,oo,0}
         };
 
+        int[][] D;
         D = floyd(G);
 
         int start = 0;
@@ -22,8 +22,16 @@ public class floyd_generic {
         System.out.println("shortest path from "+start+" to "+end+" is "+D[start][end]);
     }
 
-    public static int[][] floyd(int[][] D){
-        int n = D[0].length;
+    public static int[][] floyd(int[][] G){
+
+        // this retains the original graph, not always necessary for every problem
+        int n = G[0].length;
+        int[][] D = new int[n][n];
+        for(int x = 0; x<n; x++){
+            System.arraycopy(G[x], 0, D[x], 0, n);
+        }
+
+        // run floyd on our graph copy since it will be changing values
         for(int k = 0; k<n; k++){
             for(int x = 0; x<n; x++){
                 for(int y = 0; y<n; y++){
