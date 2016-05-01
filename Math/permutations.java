18e25c95f9466a5237023890b998b4f8f6a45778
ntubach <nikotubach@gmail.com>
2016-05-01 01:31:37 -0400

    Added permutations.java

---
+++ Math/permutations.java
@@ -0,0 +1,25 @@
+public class permutations 
+{
+	static int count = 0;
+	public static void main (String[] args)
+	{
+		String str = "12345";
+		doPermutations("", str);
+	}
+	public static void doPermutations(String pre, String str)
+	{
+		if (str.length() == 0)
+		{
+			//This outputs the permutations
+			System.out.println(pre);
+		}			
+		else
+		{
+			for(int i=0; i<str.length(); i++)
+			{
+				//Goes through each part of the string doing every permutation at each point
+				doPermutations(pre + str.charAt(i), str.substring(0, i) + str.substring(i+1, str.length()));
+			}
+		}
+	}
+}
